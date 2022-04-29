453a3692e5a02391f2750631e0933af391cb26b8
Jono Kolnik <1164060+JonathanKolnik@users.noreply.github.com>
2022-04-29 14:00:57 -0400

    add a parent level toolbar exclusion key for tabs

--- lib/ui/src/components/preview/toolbar.tsx
+++ lib/ui/src/components/preview/toolbar.tsx
@@ -226,7 +226,8 @@ export function filterTools(
     toolbarExclusions.filter((exclusionKey) => id.match(new RegExp(`^${exclusionKey}.*`)))
       .length === 0;
 
-  const filteredTabs = tabs.filter((tab) => isToolIncluded(tab.id));
+  const excludeTabs = toolbarExclusions.includes('storybook/tabs')
+  const filteredTabs = excludeTabs ? [] : tabs.filter((tab) => isToolIncluded(tab.id));
 
   const toolsLeft = [
     menuTool,
